167d6253648c6e16ecab11401540c14ef0cfc94e
Effel <99955631+EffelZefanya@users.noreply.github.com>
2026-01-26 10:42:40 +0700

    feat:created handler for grpc

--- internal/delivery/grpc/auth_handler.go
+++ internal/delivery/grpc/auth_handler.go
@@ -1,2 +1,28 @@
 package grpc
 
+import (
+	"context"
+	"reforest/pkg/pb"
+)
+
+type AuthHandler struct{
+	pb.UnimplementedAuthServiceServer
+}
+
+func NewAuthHandler() *AuthHandler{
+	return &AuthHandler{}
+}
+
+func (h *AuthHandler) Login(ctx context.Context, req *pb.LoginRequest) (*pb.AuthResponse, error){
+	return &pb.AuthResponse{
+		Token: "test",
+		Role: "role",
+	}, nil
+}
+
+func (h *AuthHandler) Register(ctx context.Context, req *pb.LoginRequest) (*pb.RegisterResponse, error){
+	return &pb.RegisterResponse{
+		Message: "succesfully registered",
+		UserId: "001",
+	}, nil
+}
